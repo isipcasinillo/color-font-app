e10c1ae296ae0a417522affd0f190d431c775436
isipcasinillo <isipcasinillo@gmail.com>
2021-09-06 14:43:33 -0500

    fixed bug with color pallete

--- public/js/create.js
+++ public/js/create.js
@@ -1,5 +1,5 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
   
     const color1 = document.querySelector('#color1').value.trim();
     const color2 = document.querySelector('#color2').value.trim();
@@ -8,7 +8,11 @@ const signupFormHandler = async (event) => {
     const font2 = document.querySelector('#font2').value.trim();
     const font3 = document.querySelector('#font3').value.trim();
 
-    if (color1 || color2 || color3 || font1 || font2 || font3 ) {
+  if (!color1 || !color2 || !color3 || !font1 || !font2 || !font3 ) {
+    alert('please input all values')
+    return;
+  } 
+  else {
       const response = await fetch('/choose', {
         method: 'POST',
         body: JSON.stringify({ color1, color2, color3, font1, font2, font3 }),
@@ -20,7 +24,7 @@ const signupFormHandler = async (event) => {
       } else {
         alert('Failed to sign up.');
       }
-    }
+  }  
 };
 
 
@@ -32,14 +36,61 @@ document
 
 // Change Color Dynamically //
 
-const selectElement = document.querySelector('.createContainer');
-
-selectElement.addEventListener('change', (event) => {
+let result = document.querySelector('.createContainer');
+result.addEventListener('change', function (event) {
   event.preventDefault();
-  $(".box1").css("backgroundColor", $(".color1").val());
-  $(".box2").css("backgroundColor", $(".color2").val());
-  $(".box3").css("backgroundColor", $(".color3").val());
-  $(".head").css("font-family", $(".font1").val());
-  $(".para").css("font-family", $(".font2").val());
-  $(".butto").css("font-family", $(".font3").val());
+    let target = event.target;
+    switch (target.id) {
+        case 'color1':
+          $(".box1").css("backgroundColor", $(".color1").val());
+            break;
+        case 'color2':
+          $(".butto").css("backgroundColor", $(".color2").val());
+            break;
+        case 'color3':
+          $(".box3").css("backgroundColor", $(".color3").val());
+            break;
+        case 'font1':
+          $(".head").css("font-family", $(".font1").val());
+            break;
+        case 'font2':
+          $(".para").css("font-family", $(".font2").val());
+            break;
+        case 'font3':
+          $(".butto").css("font-family", $(".font3").val());
+            break;
+    }
 });
+
+
+
+
+const deleteOnRow = async (event) => {
+  event.preventDefault();
+  
+    
+    $('#.delete-row', $(this).closest("td.table-id")).val()
+
+  if (rowData ) {
+    alert('please input all values')
+    return;
+  } 
+  else {
+      const response = await fetch('/delete', {
+        method: 'DELETE',
+        body: JSON.stringify({ color1}),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to sign up.');
+      }
+  }  
+};
+
+
+document
+  .querySelector('.delete-row')
+  .addEventListener('click', deleteOnRow);
